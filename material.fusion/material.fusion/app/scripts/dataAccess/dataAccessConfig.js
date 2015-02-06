847e0e2889efdbc35dd4df84e572202d9671a979
YiniYin <yiniski@gmail.com>
2015-02-06 23:31:23 +1300

    hotfix for access_mode constant

--- material.fusion/material.fusion/app/scripts/dataAccess/dataAccessConfig.js
+++ material.fusion/material.fusion/app/scripts/dataAccess/dataAccessConfig.js
@@ -11,6 +11,11 @@ dataAccessConfig.constant('ACCESS_MODE', {
 	local: 'local'
 });
 
+var ACCESS_MODE = {
+	api:   'api',
+	local: 'local'
+};
+
 dataAccessConfig.constant('MODEL_DATA_ACCESS_MODE', {
 	EventCalendarModel: ACCESS_MODE.api
 	//TODO: add model access mode here
